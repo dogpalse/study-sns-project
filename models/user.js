61e527478e931b869a4904deb1031e058d1448cf
TARI <tari@Byungsangui-MacBookAir.local>
2023-02-11 14:45:32 +0900

    유저 모델 추가

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 // 유저 정보 모델
 // 이메일 / 닉네임 / 패스워드 / SNS 로그인(provider / snsId)
-class User extends Sequelize.Model {
+module.exports = class User extends Sequelize.Model {
     static init(sequelize) {
         return super.init({
             email: {
@@ -43,5 +43,3 @@ class User extends Sequelize.Model {
     // DB 관계
     static associate(db) {}
 };
-
-module.exports = User;
