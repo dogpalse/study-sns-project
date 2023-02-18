b66562c2979d2c0430ff583a850e7fdf1cb68a25
TARI <tari@Byungsangui-MacBookAir.local>
2023-02-18 14:59:38 +0900

    passport 미들웨어 등록

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv');
 const logger = require('morgan');
+const session = require('express-session');
+const passport = require('passport');
 const pageRouter = require('./routes/page');
 const { sequelize } = require('./models');
+const passportConfig = require('./passport');
 
 dotenv.config();
+passportConfig();
 
 const app = express();
 app.set('port', process.env.PORT || 8080);
@@ -38,7 +42,17 @@ app.use((err, req, res, next) => {
     res.locals.error = process.env.NODE_ENV === 'production' ? err : {};
     res.status(err.status || 500);
     res.sendFile(path.join(__dirname, 'views/error.html'));
-})
+});
+
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: false
+    }
+}))
 
 app.listen(app.get('port'), () => {
     console.log(`${app.get('port')}에 연결...!`);
