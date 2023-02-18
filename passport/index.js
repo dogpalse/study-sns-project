7d598051aafb95b3e1816b1a3e210ab02269219d
TARI <tari@Byungsangui-MacBookAir.local>
2023-02-18 15:28:34 +0900

    passport.serializeUser와 passport.deserializeUser 정의

--- passport/index.js
+++ passport/index.js
@@ -4,6 +4,8 @@ const kakao = require('./kakaoStrategy');
 const User = require('models/User');
 
 module.exports = () => {
+    // 로그인 요청에 실행
+    // req.session 객체에 저장할 데이터 설정
     passport.serializeUser((user, done) => {
         done(null, user.id);
     });
